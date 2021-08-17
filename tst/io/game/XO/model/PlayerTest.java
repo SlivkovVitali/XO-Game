361b6178f2035df0d9296c06852c903d5a16b4e5
Vitali Slivkov <slivkov.vitali@gmail.com>
2021-08-17 11:21:23 +0300

    PlayerTest - created

---
+++ tst/io/game/XO/model/PlayerTest.java
@@ -0,0 +1,33 @@
+package io.game.XO.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getName() {
+        final String inputValue = "Vitali";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void getFigure() {
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);
+
+    }
+}

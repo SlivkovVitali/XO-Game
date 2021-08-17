5ff75648a4d1b134628f494ce8ab66ca5adde77c
Vitali Slivkov <slivkov.vitali@gmail.com>
2021-08-17 15:25:11 +0300

    WinnerController - created

---
+++ src/io/game/XO/controllers/WinnerController.java
@@ -0,0 +1,43 @@
+package io.game.XO.controllers;
+
+import io.game.XO.model.Field;
+import io.game.XO.model.Figure;
+import io.game.XO.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class WinnerController {
+    public Figure getWinner(final Field field) {
+        try {
+            for (int i = 0; i < 3; i ++)
+                if ( check(field, new Point(i, 0), new Point(i, 1), new Point(i, 2)) )
+                    return field.getFigure(new Point(i, 0));
+
+            for (int i = 0; i < 3; i ++)
+                if ( check(field, new Point(0, i), new Point(0, i), new Point(0, i)) )
+                    return field.getFigure(new Point(0, i));
+
+            if ( check(field, new Point(0, 0), new Point(1, 1), new Point(2, 2)) )
+                return field.getFigure(new Point(1, 1));
+
+            if ( check(field, new Point(2, 0), new Point(1, 1), new Point(0, 2)) )
+                return field.getFigure(new Point(1, 1));
+
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+        private boolean check(final Field field, final Point p1, final Point p2, final Point p3) {
+            try {
+                if ( field.getFigure(p1) == null ) return false;
+                if ( field.getFigure(p1) == field.getFigure(p2) &&
+                        field.getFigure(p1) == field.getFigure(p3) )
+                    return true;
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+}
